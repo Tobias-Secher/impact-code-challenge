188163bae5254bae5eb0ccca254d72c7516b94ee
Tobias Secher <tobias.secher@gmail.com>
2023-06-05 17:04:51 +0200

    Added detail page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: HomeComponent,
+  },
+  {
+    path: 'detail/:id',
+    pathMatch: 'full',
+    component: DetailComponent,
   },
 ];
 
