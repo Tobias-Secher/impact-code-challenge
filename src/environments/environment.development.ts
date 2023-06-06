38deac50b01ab8035dab1a95d6f7d2e94992dbbd
Tobias Secher <tobias.secher@gmail.com>
2023-06-06 22:09:55 +0200

    Added firebase

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,11 @@
-import { Environment } from './env.model';
-
-export const environment: Environment = {
+export const environment = {
+  firebase: {
+    projectId: 'impact-635e1',
+    appId: '1:718686556108:web:175ebbaa011acd9f4a9769',
+    storageBucket: 'impact-635e1.appspot.com',
+    apiKey: 'AIzaSyAtR7kNKudFPepWeUuv7T3r7Nv_i_led3I',
+    authDomain: 'impact-635e1.firebaseapp.com',
+    messagingSenderId: '718686556108',
+  },
   apiUrl: 'https://api.punkapi.com/v2',
 };
