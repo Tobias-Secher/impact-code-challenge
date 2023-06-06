dd2b133b737a769a3dd6cf17103256d90028e490
Tobias Secher <tobias.secher@gmail.com>
2023-06-06 21:53:50 +0200

    Added detailed view

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ImpactBrewsApiService } from '../shared/services/impact-brews-api.service';
+import { Observable, Subject, takeUntil } from 'rxjs';
+import { IBeer } from '../models/beer';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.scss']
+  styleUrls: ['./detail.component.scss'],
 })
-export class DetailComponent {
+export class DetailComponent implements OnDestroy {
+  private destroy$ = new Subject<void>();
+  beer: IBeer | undefined;
 
+  constructor(
+    private service: ImpactBrewsApiService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.service
+      .getBeer(this.activatedRoute.snapshot.params['id'])
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res) => {
+        this.beer = res[0];
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
+  }
 }
