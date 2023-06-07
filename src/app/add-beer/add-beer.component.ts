ff7af10d6b7ec81e7b10fc905fe52743a574852c
Tobias Secher <tobias.secher@gmail.com>
2023-06-07 20:15:32 +0200

    Added route + basic design

---
+++ src/app/add-beer/add-beer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-beer',
+  templateUrl: './add-beer.component.html',
+  styleUrls: ['./add-beer.component.scss']
+})
+export class AddBeerComponent {
+
+}
