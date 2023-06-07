143e432bf0d646967d2a832237b71778de25fdeb
Tobias Secher <tobias.secher@gmail.com>
2023-06-07 19:15:41 +0200

    Added search on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ImpactBrewsApiService } from '../shared/services/impact-brews-api.service';
 import { IBeer } from '../models/beer';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +11,23 @@ import { IBeer } from '../models/beer';
 })
 export class HomeComponent {
   beers$: Promise<IBeer[]>;
+  searchForm: FormGroup;
 
-  constructor(private impactBrewsApiService: ImpactBrewsApiService) {
+  constructor(
+    private impactBrewsApiService: ImpactBrewsApiService,
+    private router: Router
+  ) {
     this.beers$ = this.impactBrewsApiService.getBeers(10);
+    this.searchForm = new FormGroup({
+      searchTerm: new FormControl(),
+    });
+  }
+
+  searchSubmit() {
+    this.router.navigate(['/search'], {
+      queryParams: {
+        q: this.searchForm.get('searchTerm')?.value,
+      },
+    });
   }
 }
